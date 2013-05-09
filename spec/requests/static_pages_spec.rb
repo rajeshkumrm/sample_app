1ac4be3c69e37fc121ee64753ef5eb68c457e754
Rajesh Kumar <rajesh@arrivusystems.com>
2013-05-09 17:04:42 +0530

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,13 +5,15 @@ describe "StaticPages" do
     it "should have the h1 'Sample App' " do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
+      #visit root_path
       page.should have_selector('h1', :text => 'Sample App')
     end
 
     it "should have the title 'Home' " do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+      #visit root_path
+      page.should have_selector('title', :text => 'Home')
     end
   end
 
@@ -24,7 +26,7 @@ describe "StaticPages" do
   it "should have the title 'Help' " do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/help'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+      page.should have_selector('title', :text => "Help")
     end
 end
 
@@ -35,7 +37,18 @@ describe "About page" do
   end
   it "should have the title 'About Us' " do
     visit '/static_pages/about'
-    page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
+    page.should have_selector('title', :text => "About Us")
+  end
+end
+
+describe "Contact page" do
+  it "should have the h1 'Contact' " do
+    visit '/static_pages/contact'
+    page.should have_selector('h1', :text => 'Contact')
+  end
+  it "should have the title 'Contact' " do
+    visit '/static_pages/contact'
+    page.should have_selector('title', :text => "Contact")
   end
 end
 
